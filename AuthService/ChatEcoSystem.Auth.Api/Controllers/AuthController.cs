467ed74f419a601077c42d642efb1b7cf00feffe
Александр <@DESKTOP-L9V170T>
2025-07-02 15:22:40 +0300

    Контроллер авторизации

--- AuthService/ChatEcoSystem.Auth.Api/Controllers/AuthController.cs
+++ AuthService/ChatEcoSystem.Auth.Api/Controllers/AuthController.cs
@@ -1,25 +1,37 @@
 ﻿using System.Threading.Tasks;
 using ChatEcoSystem.Auth.Logic;
 using ChatEcoSystem.SharedLib.Contracts;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChatEcoSystem.Auth.Api.Controllers
 {
+    /// <summary>
+    /// Контроллер авторизации пользователя
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController : BaseApiController
     {
+        #region CTOR
+        /// <inheritdoc cref="IAuthService"/>
         private readonly IAuthService _authService;
         public AuthController(IAuthService authService)
         {
             _authService = authService;
         }
+        #endregion
 
-
+        /// <summary>
+        /// Авторизация пользователя
+        /// </summary>
+        /// <param name="request">Запрос авторизации <see cref="UserAuthRequestDto"/></param>
         [HttpPost(nameof(Login))]
-        public async Task<BaseApiResponse<UserAuthResponseDto>> Login([FromBody] UserAuthRequestDto request)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BaseApiResponse<UserAuthResponseDto>))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(BaseApiResponse<UserAuthResponseDto>))]
+        public async Task<IActionResult> Login([FromBody] UserAuthRequestDto request)
         {
-            return await _authService.Authenticate(request);
+            return ActionRes(await _authService.Authenticate(request));
         }
     }
 }
